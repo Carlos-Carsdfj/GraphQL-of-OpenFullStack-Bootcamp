e31d828264c875de94ec269729e8ec7ab74546cb
Carlos-Carsdfj <carsdfj@gmail.com>
2021-09-10 12:45:22 -0400

    Fix react-with-apollo app

--- react-with-apollo/src/components/Persons.js
+++ react-with-apollo/src/components/Persons.js
@@ -1,5 +1,31 @@
+import { useState, useEffect } from'react'
+import { useLazyQuery } from '@apollo/client';
+import { FIND_PERSON } from '../util/query.js'
 const Persons = ({ persons }) =>{
 
+  const [ getPerson, result ] = useLazyQuery(FIND_PERSON)
+  const [ person, setPerson ] = useState(null)
+
+  const showPerson = (name) => {
+    getPerson({variables: { nameToSearch: name }})
+  }
+
+  useEffect(() => {
+      if (result.data) {
+        setPerson(result.data.findPerson)
+      }
+  },[result])
+  
+  if(person) {
+    return(
+      <div>
+        <h2>{person.name}</h2>
+        <div>{person.address.street} {person.address.city}</div>
+        <div>{person.phone}</div>
+        <button onClick={() => setPerson(null)}>close</button>
+      </div>  
+    )
+  }
   return (
     <div>
       <h2>Persons</h2>
@@ -7,6 +33,9 @@ const Persons = ({ persons }) =>{
         persons.map( p => 
           <div key={p.id}>
           {p.name} : {p.phone}
+          <button onClick={() => {showPerson(p.name)}}>
+          show address
+          </button>
           </div>
         )
       }
